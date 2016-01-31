e764003ffa992c25f7eb7e5ba74d708b4a609855
Tiago Fernandes <tiago.fernandes@enear.co>
2016-01-31 10:24:11 +0000

    refactor: update several bower components and get rid of unused angular dependencies

--- app/app.js
+++ app/app.js
@@ -39,5 +39,27 @@
                     templateUrl: 'components/home/proverb-single/proverb-single.html',
                     controller: 'ProverbSingleCtrl as vm'
                 });
+        })
+        .run(function() {
+            /* Keyboard controls
+             *
+             * left arrow	37
+             * up arrow	    38
+             * right arrow	39
+             * down arrow	40
+             *
+             * */
+
+            // Hash code converter for our string objects
+            String.prototype.hashCode = function() {
+                var hash = 0, i, chr, len;
+                if (this.length == 0) return hash;
+                for (i = 0, len = this.length; i < len; i++) {
+                    chr   = this.charCodeAt(i);
+                    hash  = ((hash << 5) - hash) + chr;
+                    hash |= 0; // Convert to 32bit integer
+                }
+                return hash;
+            };
         });
 })();
